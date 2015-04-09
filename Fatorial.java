20c1436a908b8bb962d69e90f3497974c9ab91bf
Bruno Bitencourt Luiz <bluiz222@gmail.com>
2015-04-09 19:36:42 -0300

    Removi syso

--- Fatorial.java
+++ Fatorial.java
@@ -1,59 +1,7 @@
 public class Fatorial {
  
  public static void main(String []args) {
-      System.out.println("Fatorial");
-      System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
-  System.out.println("Fatorial");
+      System.out.println("Fdasdasatorial");
  }
 
 }
